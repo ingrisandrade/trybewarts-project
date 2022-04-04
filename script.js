4935c26417f1cdc01c9e25af44f44aa60923480e
Ingris Andrade <iac22@outlook.com>
2022-04-04 12:12:56 -0300

    Refatoração do código

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-// Ao preencher o formulário e clicar no botão, será validado que:
-// Caso o email seja "tryber@teste.com" e a senha seja "123456" será emitido um alerta contendo o texto "Olá, Tryber!"
-// Em todos os outro casos deverá ser emitido um alerta contendo o texto "Email ou senha inválidos."
-
 const buttonForm = document.querySelector('#button-form');
+const inputEmail = document.querySelector('#email');
+const inputSenha = document.querySelector('#senha');
+const contador = document.querySelector('#counter');
+const textarea = document.querySelector('#textarea');
+const btnSubmit = document.querySelector('#submit-btn');
+const inputCheckbox = document.querySelector('#agreement');
 
+// Valida o botão: Se email= tryber@teste.com e senha= 123456, emite alert com o texto: "Olá, Tryber!".
+// Senão, emite alert com o texto: "Email ou senha inválidos."
 buttonForm.addEventListener('click', () => {
-  const inputEmail = document.querySelector('#email').value;
-  const inputSenha = document.querySelector('#senha').value;
-
-  if (inputEmail === 'tryber@teste.com' && inputSenha === '123456') {
+  if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
     alert('Olá, Tryber!');
   }
   alert('Email ou senha inválidos.');
 });
 
-const contador = document.querySelector('#counter');
-const textarea = document.querySelector('#textarea');
-
+// Conta os caracteres adicionados pelo o usuário no textArea.
 function contadorDeCaracter() {
   contador.innerText = 500 - textarea.value.length;
 }
 textarea.addEventListener('keyup', contadorDeCaracter);
 
-const btnSubmit = document.querySelector('#submit-btn');
-const inputCheckbox = document.querySelector('#agreement');
-
+// Só habilita o botão se o checkbox estiver for igual a checked.
 function habilitaBotao() {
   btnSubmit.disabled = !inputCheckbox.checked;
 }
-
 inputCheckbox.addEventListener('change', habilitaBotao);
 
-// // Faça com que, ao clicar no botão Enviar, o conteúdo dentro da tag <form> seja substituído pelas informações preenchidas pela pessoa usuária
-// Observações técnicas:
-// Todos os campos do formulário devem ser substituídos pelas informações da pessoa usuária.
-// Deve haver um campo com o formato "Nome: Alguem Aqui"
-// Deve haver um campo com o formato "Email: email@mail.com"
-// Deve haver um campo com o formato "Casa: Casa Escolhida"
-// Deve haver um campo com o formato "Família: Família Escolhida"
-// Deve haver um campo com o formato "Matérias: Matérias, Marcadas, Aqui"
-// Deve haver um campo com o formato "Avaliação: NotaAqui"
-// Deve haver um campo com o formato "Observações: Observações aqui"
-
-// btnSubmit.addEventListener('click', )
+// Algumas classas e ids foram adicionadas no html para o requisito 21
